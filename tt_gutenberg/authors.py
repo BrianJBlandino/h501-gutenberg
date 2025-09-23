0727cf585d0acc8ca42942cbc262dcce35ae1c9e
BrianJBlandino <bblandi@iu.edu>
2025-09-23 08:25:03 -0400

    starting building function

--- tt_gutenberg/authors.py
+++ tt_gutenberg/authors.py
@@ -1,1 +1,5 @@
-    
+def list_authors(x,y):
+    import pandas as pd
+    import numpy as np
+    df = pd.read.csv(r'C:\Users\renai\OneDrive\College\Info 501 Introduction to Data Science Programming\GitHub\h501-gutenberg\gutenberg_authors.csv')
+    df
