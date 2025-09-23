045bd52dbb3691fc2ae9ec063f997b0965c4f188
BrianJBlandino <bblandi@iu.edu>
2025-09-23 13:27:10 +0000

    figuring out why df isn't creating

--- tt_gutenberg/authors.py
+++ tt_gutenberg/authors.py
@@ -2,4 +2,4 @@ def load_data():
     import pandas as pd
     import numpy as np
     df = pd.read_csv(r'C:\Users\renai\OneDrive\College\Info 501 Introduction to Data Science Programming\GitHub\h501-gutenberg\gutenberg_authors.csv')
-    df
+    print(df)
